441c01cda29728d69abea3b318319212cc493a27
Prof Program <210163492@aston.ac.uk>
2025-02-24 20:15:50 +0000

    lab2
    node is giving "/bin/sh: 59: node: not found" error when running "node func_as_arg.js" inside lab2-functionalprimer/javascript

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,9 +1,23 @@
 --create inpFunc
-inpFunc = [1..5] 
+inpFunc :: Int -> Int -> [Int]
+inpFunc a b = [a..b] 
 
 --Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+applicatorFunc :: (Int -> Int -> [Int]) -> Char -> Int -> Int -> Double
+applicatorFunc fun s a b
+        | s == 's' = fromIntegral (sum nums)
+        | otherwise = fromIntegral (sum nums)/ fromIntegral b
+        where nums = fun a b  
 
 main = do
-    let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
+    putStrLn "Enter the start number (a):"
+    aInput <- getLine
+    putStrLn "Enter the end number (b):"
+    bInput <- getLine
+    putStrLn "Enter 's' for sum or any other character for average:"
+    sInput <- getLine
+    let a = read aInput :: Int
+        b = read bInput :: Int
+        s = if null sInput then ' ' else head sInput
+    let result = applicatorFunc inpFunc s a b--Call applicatorFunc with inpFunc and 'a' as args
     putStrLn("sum = " ++ show(result))
